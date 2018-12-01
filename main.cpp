ae687b80400f4f71533a969e6c56352b5febe2d0
Just Wonder Blue <justwonderblue@gmail.com>
2018-12-01 01:52:54 -0500

    Used kevacoin seeders.

--- main.cpp
+++ main.cpp
@@ -397,7 +397,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
+static const string mainnet_seeds[] = {"dnsseed.kevacoin.org", "dnsseed.kevacoin.com"};
 static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
                                        "testnet-seed.bitcoin.petertodd.org",
                                        "testnet-seed.bluematt.me",
